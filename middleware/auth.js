6ed50f07b90108324a42e38749ece525b2a79f26
KonstantinModin <konstantin.v.modin@gmail.com>
2020-03-12 07:08:12 +0100

    updates

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = (req, res, next) => {
+    // Get token from header
+    const token = req.header("x-auth-token");
+
+    // Check if not token
+    if (!token) {
+        return res.status(401).json({ msg: "No token, authorization denied" });
+    }
+
+    //Verify token
+    try {
+        const decoded = jwt.verify(token, config.get("jwtSecret"));
+        // console.log("decoded (jwt.verify)=", decoded);
+        // console.log("req.user initially =", req.user);
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        return res.status(401).json({ msg: "Token is not valid" });
+    }
+};
