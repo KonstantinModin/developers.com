574dbd0b1da608c65ec4f81d165b37b9da2bbad3
KonstantinModin <konstantin.v.modin@gmail.com>
2020-03-13 09:38:24 +0100

    updates

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -87,7 +87,7 @@ router.post(
                 profile = await Profile.findOneAndUpdate(
                     { user: req.user.id },
                     { $set: profileFields },
-                    { new: true, useFindAndModify: false }
+                    { new: true }
                 );
                 return res.json(profile);
             }
@@ -101,5 +101,41 @@ router.post(
         }
     }
 );
+
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get("/", async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate("user", [
+            "name",
+            "avatar"
+        ]);
+        res.json(profiles);
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get("/user/:user_id", async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.params.user_id
+        }).populate("user", ["name", "avatar"]);
+
+        if (!profile)
+            return res
+                .status(400)
+                .json({ msg: "There is no profile for this user" });
+        res.json(profile);
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
 
 module.exports = router;
