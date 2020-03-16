3e5987319071b5cf53a1d005f54bd5a23cc10d5b
KonstantinModin <konstantin.v.modin@gmail.com>
2020-03-16 08:01:44 +0100

    updates

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const request = require("request");
+const config = require("config");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
@@ -216,5 +218,134 @@ router.put(
         }
     }
 );
+
+// @route   DELETE api/profile/expirience/:exp_id
+// @desc    Delete expirience from profile
+// @access  Private
+
+router.delete("/expirience/:exp_id", auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        const newExpirience = profile.experience.filter(
+            item => item.id !== req.params.exp_id
+        );
+        profile.experience = newExpirience;
+        await profile.save();
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+// @route   PUT api/profile/education
+// @desc    Add profile education
+// @access  Private
+router.put(
+    "/education",
+    [
+        auth,
+        [
+            check("school", "School is required")
+                .not()
+                .isEmpty(),
+            check("degree", "Degree is required")
+                .not()
+                .isEmpty(),
+            check("fieldofstudy", "Field of study is required")
+                .not()
+                .isEmpty(),
+            check("from", "From is required")
+                .not()
+                .isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        console.log(req.body);
+        const {
+            school,
+            degree,
+            fieldofstudy,
+            from,
+            to,
+            current,
+            description
+        } = req.body;
+
+        const newEdu = {
+            school,
+            degree,
+            fieldofstudy,
+            from,
+            to,
+            current,
+            description
+        };
+
+        try {
+            const profile = await Profile.findOne({ user: req.user.id });
+            profile.education.unshift(newEdu);
+            await profile.save();
+            res.json(profile);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Server Error");
+        }
+    }
+);
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Delete education from profile
+// @access  Private
+
+router.delete("/education/:edu_id", auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        profile.education = profile.education.filter(
+            item => item.id !== req.params.edu_id
+        );
+
+        await profile.save();
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from Github
+// @access  Public
+
+router.get("/github/:username", (req, res) => {
+    try {
+        const options = {
+            uri: encodeURI(
+                `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+            ),
+            method: "GET",
+            headers: {
+                "user-agent": "node.js",
+                Authorization: `token ${config.get("gitToken")}`
+            }
+        };
+        request(options, (error, response, body) => {
+            if (error) console.error(error);
+            if (response.statusCode !== "200") {
+                res.status(404).json({ msg: "No Github profile found" });
+            }
+            res.json(JSON.parse(body));
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+});
 
 module.exports = router;
